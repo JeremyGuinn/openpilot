4cc24fd7926bcb9655a16a528f204fa39bdaa393
Martin Lillepuu <martin@mlp.ee>
2020-05-05 18:15:36 +0300

    Add steerError and steerWarning, zero apply_steer on steerWarning

--- selfdrive/car/subaru/carcontroller.py
+++ selfdrive/car/subaru/carcontroller.py
@@ -51,16 +51,7 @@ class CarController():
       apply_steer = apply_std_steer_torque_limits(new_steer, self.apply_steer_last, CS.out.steeringTorque, P)
       self.steer_rate_limited = new_steer != apply_steer
 
-      lkas_enabled = enabled
-
-      # noise generation
-      if (frame % 10) and enabled:
-        if apply_steer < 0:
-          apply_steer += 1
-        else:
-          apply_steer -= 1
-
-      if not lkas_enabled:
+      if not enabled or CS.out.steerWarning:
         apply_steer = 0
 
       can_sends.append(subarucan.create_steering_control(self.packer, apply_steer, frame, P.STEER_STEP))
